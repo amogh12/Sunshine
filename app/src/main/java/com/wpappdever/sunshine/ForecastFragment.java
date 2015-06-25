78e49df939dfa6ff035fc0bcf8872153ae4d3fdc
ASP <amogh.gates@gmail.com>
2015-06-24 21:56:46 -0500

    adding settings screen

--- app/src/main/java/com/wpappdever/sunshine/ForecastFragment.java
+++ app/src/main/java/com/wpappdever/sunshine/ForecastFragment.java
@@ -1,8 +1,10 @@
 package com.wpappdever.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.text.format.Time;
@@ -51,6 +53,12 @@ public class ForecastFragment extends Fragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
+    }
+
+    @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.forecastfragment_menu, menu);
     }
@@ -64,12 +72,18 @@ public class ForecastFragment extends Fragment {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_refresh) {
-            FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("38016");
+            updateWeather();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void updateWeather() {
+        FetchWeatherTask weatherTask = new FetchWeatherTask();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location = prefs.getString(getString(R.string.pref_key_location_key), getString(R.string.pref_location_default));
+        weatherTask.execute(location);
     }
 
     @Override
