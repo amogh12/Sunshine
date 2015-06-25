fad649d0acf946b663692aeeed00deb80d62e476
ASP <amogh.gates@gmail.com>
2015-06-24 20:44:10 -0500

    adding another activity

---
+++ app/src/main/java/com/wpappdever/sunshine/DetailActivityFragment.java
@@ -0,0 +1,34 @@
+package com.wpappdever.sunshine;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailActivityFragment extends Fragment {
+
+    public DetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Intent intent = getActivity().getIntent();
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+            Log.v("FetchWeatherTask", "FOrecast is " +forecast);
+            ((TextView) rootView.findViewById(R.id.detailText))
+                    .setText(forecast);
+        }
+
+        return rootView;
+    }
+}
--- app/src/main/java/com/wpappdever/sunshine/ForecastFragment.java
+++ app/src/main/java/com/wpappdever/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.wpappdever.sunshine;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
@@ -13,8 +14,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -92,6 +95,16 @@ public class ForecastFragment extends Fragment {
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
 
+        //handle click listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                      .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
